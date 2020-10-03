c64322bbeedf1fd738384dd79e9c776cf89f1481
Sneha Motwani <snehamotwani11@gmail.com>
2020-10-03 16:34:25 +0530

    Added gitignore and git attributes file

--- BrainWorks.Nest.Data/Program.cs
+++ BrainWorks.Nest.Data/Program.cs
@@ -6,7 +6,7 @@ namespace BrainWorks.Nest.Data
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Project added to existing repo");
         }
     }
 }
